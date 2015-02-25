a14670f2aaf4efcca5ab782ce0c5cb7f273b423f
Bao Xiang <xiang.bao.frank@gmail.com>
2015-02-25 14:02:32 +0800

    first commit

--- pyScoliosis.py
+++ pyScoliosis.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-#!/bin/env python
+# !/bin/env python
 import wx
+
 import pyScoliosisUI as ui
+
 
 if "2.8" in wx.version():
     import wx.lib.pubsub.setupkwargs
@@ -9,11 +11,12 @@ if "2.8" in wx.version():
 else:
     from wx.lib.pubsub import pub
 
+
 class LoginDialog():
-    def __init__(self,parent):
-        ui.LoginDialogBase.__init__(self,parent)
-    
-    def login(self,event):
+    def __init__(self, parent):
+        ui.LoginDialogBase.__init__(self, parent)
+
+    def login(self, event):
         stupid_password = "password"
         user_password = self.txt_password.GetValue()
         if user_password == stupid_password:
@@ -24,22 +27,12 @@ class LoginDialog():
             print "Username or password is incorrect!"
             self.Destroy()
 
-    def cancel(self,event):
+    def cancel(self, event):
         self.Destroy()
 
-#class MainFrame():
-#    def __init__(self):
-  #      mainForm.__init__(self)
-        #pub.subscribe(self.myListener, "frameListener")
-       # dlg = LoginDialog(None)
-       # dlg.ShowModal()
-
-    #def myListener(self, message, arg2=None):
-     #   self.Show()
 
 if __name__ == '__main__':
     app = wx.App()
     frame = ui.mainForm(None)
     frame.Show(True)
-    #print frame.txtSchool.GetValue()
     app.MainLoop()
--- pyScoliosisUI.py
+++ pyScoliosisUI.py
@@ -15,128 +15,136 @@ import wx.grid
 ## Class mainForm
 ###########################################################################
 
-class mainForm ( wx.Frame ):
-	
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"脊柱侧凸预防干预信息系统", pos = wx.DefaultPosition, size = wx.Size( 800,600 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-		
-		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
-		
-		self.m_statusBar1 = self.CreateStatusBar( 1, wx.ST_SIZEGRIP, wx.ID_ANY )
-		self.m_menubar1 = wx.MenuBar( 0 )
-		self.fileMenu = wx.Menu()
-		self.m_menubar1.Append( self.fileMenu, u"文件" ) 
-		
-		self.SetMenuBar( self.m_menubar1 )
-		
-		bSizer1 = wx.BoxSizer( wx.HORIZONTAL )
-		
-		self.patientDataTable = wx.grid.Grid( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.RAISED_BORDER|wx.VSCROLL )
-		
-		# Grid
-		self.patientDataTable.CreateGrid( 5, 7 )
-		self.patientDataTable.EnableEditing( True )
-		self.patientDataTable.EnableGridLines( True )
-		self.patientDataTable.EnableDragGridSize( False )
-		self.patientDataTable.SetMargins( 0, 0 )
-		
-		# Columns
-		self.patientDataTable.EnableDragColMove( False )
-		self.patientDataTable.EnableDragColSize( True )
-		self.patientDataTable.SetColLabelSize( 30 )
-		self.patientDataTable.SetColLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
-		
-		# Rows
-		self.patientDataTable.EnableDragRowSize( True )
-		self.patientDataTable.SetRowLabelSize( 80 )
-		self.patientDataTable.SetRowLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
-		
-		# Label Appearance
-		
-		# Cell Defaults
-		self.patientDataTable.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
-		bSizer1.Add( self.patientDataTable, 1, wx.ALL|wx.EXPAND, 0 )
-		
-		self.operationPanel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.operationPanel.SetMaxSize( wx.Size( 350,-1 ) )
-		
-		gbSizer1 = wx.GridBagSizer( 0, 0 )
-		gbSizer1.SetFlexibleDirection( wx.HORIZONTAL )
-		gbSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-		
-		self.lblSchool = wx.StaticText( self.operationPanel, wx.ID_ANY, u"学校：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblSchool.Wrap( -1 )
-		gbSizer1.Add( self.lblSchool, wx.GBPosition( 1, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtSchool = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtSchool, wx.GBPosition( 1, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblClass = wx.StaticText( self.operationPanel, wx.ID_ANY, u"班级：", wx.Point( -1,-1 ), wx.DefaultSize, 0 )
-		self.lblClass.Wrap( -1 )
-		gbSizer1.Add( self.lblClass, wx.GBPosition( 2, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtClass = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtClass, wx.GBPosition( 2, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblStuNum = wx.StaticText( self.operationPanel, wx.ID_ANY, u"学号：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblStuNum.Wrap( -1 )
-		gbSizer1.Add( self.lblStuNum, wx.GBPosition( 3, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtStuNum = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtStuNum, wx.GBPosition( 3, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblName = wx.StaticText( self.operationPanel, wx.ID_ANY, u"姓名：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblName.Wrap( -1 )
-		gbSizer1.Add( self.lblName, wx.GBPosition( 4, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtName = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtName, wx.GBPosition( 4, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblContactInfo = wx.StaticText( self.operationPanel, wx.ID_ANY, u"联系方式：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblContactInfo.Wrap( -1 )
-		gbSizer1.Add( self.lblContactInfo, wx.GBPosition( 5, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtContactInfo = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtContactInfo, wx.GBPosition( 5, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblMeasuredAngle = wx.StaticText( self.operationPanel, wx.ID_ANY, u"测量角度：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblMeasuredAngle.Wrap( -1 )
-		gbSizer1.Add( self.lblMeasuredAngle, wx.GBPosition( 6, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtMeasuredAngle = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtMeasuredAngle, wx.GBPosition( 6, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.lblDistrict = wx.StaticText( self.operationPanel, wx.ID_ANY, u"区域：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.lblDistrict.Wrap( -1 )
-		gbSizer1.Add( self.lblDistrict, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.txtDistrict = wx.TextCtrl( self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.txtDistrict, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
-		
-		self.btnSearch = wx.Button( self.operationPanel, wx.ID_ANY, u"查找", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.btnSearch, wx.GBPosition( 7, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		self.cbxFilter = wx.CheckBox( self.operationPanel, wx.ID_ANY, u"只显示未检查病人", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.cbxFilter, wx.GBPosition( 8, 0 ), wx.GBSpan( 1, 2 ), wx.ALL, 5 )
-		
-		self.btnExport = wx.Button( self.operationPanel, wx.ID_ANY, u"导出当前数据", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer1.Add( self.btnExport, wx.GBPosition( 9, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 5 )
-		
-		
-		gbSizer1.AddGrowableCol( 1 )
-		
-		self.operationPanel.SetSizer( gbSizer1 )
-		self.operationPanel.Layout()
-		gbSizer1.Fit( self.operationPanel )
-		bSizer1.Add( self.operationPanel, 1, wx.ALL|wx.EXPAND, 0 )
-		
-		
-		self.SetSizer( bSizer1 )
-		self.Layout()
-		
-		self.Centre( wx.BOTH )
-	
-	def __del__( self ):
-		pass
+class mainForm(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"脊柱侧凸预防干预信息系统", pos=wx.DefaultPosition,
+                          size=wx.Size(800, 600), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
+
+        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+
+        self.m_statusBar1 = self.CreateStatusBar(1, wx.ST_SIZEGRIP, wx.ID_ANY)
+        self.m_menubar1 = wx.MenuBar(0)
+        self.fileMenu = wx.Menu()
+        self.m_menubar1.Append(self.fileMenu, u"文件")
+
+        self.SetMenuBar(self.m_menubar1)
+
+        bSizer1 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.patientDataTable = wx.grid.Grid(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize,
+                                             wx.RAISED_BORDER | wx.VSCROLL)
+
+        # Grid
+        self.patientDataTable.CreateGrid(5, 7)
+        self.patientDataTable.EnableEditing(True)
+        self.patientDataTable.EnableGridLines(True)
+        self.patientDataTable.EnableDragGridSize(False)
+        self.patientDataTable.SetMargins(0, 0)
+
+        # Columns
+        self.patientDataTable.EnableDragColMove(False)
+        self.patientDataTable.EnableDragColSize(True)
+        self.patientDataTable.SetColLabelSize(30)
+        self.patientDataTable.SetColLabelAlignment(wx.ALIGN_CENTRE, wx.ALIGN_CENTRE)
+
+        # Rows
+        self.patientDataTable.EnableDragRowSize(True)
+        self.patientDataTable.SetRowLabelSize(80)
+        self.patientDataTable.SetRowLabelAlignment(wx.ALIGN_CENTRE, wx.ALIGN_CENTRE)
+
+        # Label Appearance
+
+        # Cell Defaults
+        self.patientDataTable.SetDefaultCellAlignment(wx.ALIGN_LEFT, wx.ALIGN_TOP)
+        bSizer1.Add(self.patientDataTable, 1, wx.ALL | wx.EXPAND, 0)
+
+        self.operationPanel = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        self.operationPanel.SetMaxSize(wx.Size(350, -1))
+
+        gbSizer1 = wx.GridBagSizer(0, 0)
+        gbSizer1.SetFlexibleDirection(wx.HORIZONTAL)
+        gbSizer1.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        self.lblSchool = wx.StaticText(self.operationPanel, wx.ID_ANY, u"学校：", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lblSchool.Wrap(-1)
+        gbSizer1.Add(self.lblSchool, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtSchool = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                     0)
+        gbSizer1.Add(self.txtSchool, wx.GBPosition(1, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblClass = wx.StaticText(self.operationPanel, wx.ID_ANY, u"班级：", wx.Point(-1, -1), wx.DefaultSize, 0)
+        self.lblClass.Wrap(-1)
+        gbSizer1.Add(self.lblClass, wx.GBPosition(2, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtClass = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                    0)
+        gbSizer1.Add(self.txtClass, wx.GBPosition(2, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblStuNum = wx.StaticText(self.operationPanel, wx.ID_ANY, u"学号：", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lblStuNum.Wrap(-1)
+        gbSizer1.Add(self.lblStuNum, wx.GBPosition(3, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtStuNum = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                     0)
+        gbSizer1.Add(self.txtStuNum, wx.GBPosition(3, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblName = wx.StaticText(self.operationPanel, wx.ID_ANY, u"姓名：", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lblName.Wrap(-1)
+        gbSizer1.Add(self.lblName, wx.GBPosition(4, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtName = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                   0)
+        gbSizer1.Add(self.txtName, wx.GBPosition(4, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblContactInfo = wx.StaticText(self.operationPanel, wx.ID_ANY, u"联系方式：", wx.DefaultPosition,
+                                            wx.DefaultSize, 0)
+        self.lblContactInfo.Wrap(-1)
+        gbSizer1.Add(self.lblContactInfo, wx.GBPosition(5, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtContactInfo = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
+                                          wx.DefaultSize, 0)
+        gbSizer1.Add(self.txtContactInfo, wx.GBPosition(5, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblMeasuredAngle = wx.StaticText(self.operationPanel, wx.ID_ANY, u"测量角度：", wx.DefaultPosition,
+                                              wx.DefaultSize, 0)
+        self.lblMeasuredAngle.Wrap(-1)
+        gbSizer1.Add(self.lblMeasuredAngle, wx.GBPosition(6, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtMeasuredAngle = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
+                                            wx.DefaultSize, 0)
+        gbSizer1.Add(self.txtMeasuredAngle, wx.GBPosition(6, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.lblDistrict = wx.StaticText(self.operationPanel, wx.ID_ANY, u"区域：", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lblDistrict.Wrap(-1)
+        gbSizer1.Add(self.lblDistrict, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.txtDistrict = wx.TextCtrl(self.operationPanel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
+                                       wx.DefaultSize, 0)
+        gbSizer1.Add(self.txtDistrict, wx.GBPosition(0, 1), wx.GBSpan(1, 1), wx.ALL | wx.EXPAND, 5)
+
+        self.btnSearch = wx.Button(self.operationPanel, wx.ID_ANY, u"查找", wx.DefaultPosition, wx.DefaultSize, 0)
+        gbSizer1.Add(self.btnSearch, wx.GBPosition(7, 1), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        self.cbxFilter = wx.CheckBox(self.operationPanel, wx.ID_ANY, u"只显示未检查病人", wx.DefaultPosition, wx.DefaultSize, 0)
+        gbSizer1.Add(self.cbxFilter, wx.GBPosition(8, 0), wx.GBSpan(1, 2), wx.ALL, 5)
+
+        self.btnExport = wx.Button(self.operationPanel, wx.ID_ANY, u"导出当前数据", wx.DefaultPosition, wx.DefaultSize, 0)
+        gbSizer1.Add(self.btnExport, wx.GBPosition(9, 1), wx.GBSpan(1, 1), wx.ALL, 5)
+
+        gbSizer1.AddGrowableCol(1)
+
+        self.operationPanel.SetSizer(gbSizer1)
+        self.operationPanel.Layout()
+        gbSizer1.Fit(self.operationPanel)
+        bSizer1.Add(self.operationPanel, 1, wx.ALL | wx.EXPAND, 0)
+
+        self.SetSizer(bSizer1)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+    def __del__(self):
+        pass
 	
 
